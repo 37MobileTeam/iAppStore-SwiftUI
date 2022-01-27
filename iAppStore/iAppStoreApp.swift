cafe68733aed12ab34df5cf41e640c5aaaae3454
peak <peak@jiemogame.com>
2022-01-27 10:15:57 +0800

    fix bug: navitate to app detail view and back to rank home, tabbar background become tranparent

--- iAppStore/iAppStoreApp.swift
+++ iAppStore/iAppStoreApp.swift
@@ -15,28 +15,32 @@ struct iAppStoreApp: App {
         setupApperance()
     }
     
-    var body: some Scene {
-        WindowGroup {
-            TabbarView().accentColor(.tsmg_blue)
-        }
-    }
-    
     private func setupApperance() {
         UINavigationBar.appearance().largeTitleTextAttributes = [
-            NSAttributedString.Key.foregroundColor: UIColor(named: "tsmg_blue")!]
+            NSAttributedString.Key.foregroundColor: UIColor(named: "tsmg_blue")!
+        ]
         
         UINavigationBar.appearance().titleTextAttributes = [
-            NSAttributedString.Key.foregroundColor: UIColor(named: "tsmg_blue")!]
+            NSAttributedString.Key.foregroundColor: UIColor(named: "tsmg_blue")!
+        ]
         
         UIBarButtonItem.appearance().setTitleTextAttributes([
             NSAttributedString.Key.foregroundColor: UIColor(named: "tsmg_blue")!,
         ], for: .normal)
         
         UIWindow.appearance().tintColor = UIColor(named: "tsmg_blue")
+        
+        // set tabbar background color
+        UITabBar.appearance().backgroundColor = UIColor(Color.white)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            TabbarView()
+                .accentColor(.tsmg_blue)
+        }
     }
 }
-
-
 
 // MARK: - iOS implementation
 struct TabbarView: View {
@@ -56,18 +60,29 @@ struct TabbarView: View {
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            RankHome().tabItem{
-                self.tabbarItem(text: "榜单", image: "arrow.up.arrow.down.square")
-            }.tag(Tab.rankLists)
-            SearchHome().tabItem{
-                self.tabbarItem(text: "搜索", image: "magnifyingglass.circle.fill")
-            }.tag(Tab.search)
-            SubscriptionHome().tabItem{
-                self.tabbarItem(text: "订阅", image: "checkmark.seal.fill")
-            }.tag(Tab.subscription)
-            SettingHome().tabItem{
-                self.tabbarItem(text: "设置", image: "heart.circle")
-            }.tag(Tab.setting)
+            RankHome()
+                .tabItem {
+                    tabbarItem(text: "榜单", image: "arrow.up.arrow.down.square")
+                }
+                .tag(Tab.rankLists)
+            
+            SearchHome()
+                .tabItem {
+                    tabbarItem(text: "搜索", image: "magnifyingglass.circle.fill")
+                }
+                .tag(Tab.search)
+           
+            SubscriptionHome()
+                .tabItem {
+                    tabbarItem(text: "订阅", image: "checkmark.seal.fill")
+                }
+                .tag(Tab.subscription)
+            
+            SettingHome()
+                .tabItem {
+                    tabbarItem(text: "设置", image: "heart.circle")
+                }
+                .tag(Tab.setting)
         }
     }
 }
